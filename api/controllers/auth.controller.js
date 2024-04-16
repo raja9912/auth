79c3a1d88146e2bcc755fc3ff353652f75f60f91
raja9912 <rajasimhareddy02.com>
2024-04-15 23:34:43 -0500

    add sihnout user functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,15 +2,13 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
-
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
-
   try {
     await newUser.save();
-    res.status(201).json({ message: "user created successfully" });
+    res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     next(error);
   }
@@ -22,9 +20,9 @@ export const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(404, "User not found"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "wrong credentials"));
-    const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: hashedPassword, ...rest } = validUser._doc;
-    const expiryDate = new Date(Date.now() + 3600000); //1 hour
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
     res
       .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
       .status(200)
@@ -33,16 +31,18 @@ export const signin = async (req, res, next) => {
     next(error);
   }
 };
-
 export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
     if (user) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: hashedPassword, ...rest } = user._doc;
-      const expiryDate = new Date(Date.now() + 3600000); //1 hour
+      const expiryDate = new Date(Date.now() + 3600000); // 1 hour
       res
-        .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+        .cookie("access_token", token, {
+          httpOnly: true,
+          expires: expiryDate,
+        })
         .status(200)
         .json(rest);
     } else {
@@ -61,7 +61,7 @@ export const google = async (req, res, next) => {
       await newUser.save();
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password: hashedPassword2, ...rest } = newUser._doc;
-      const expiryDate = new Date(Date.now() + 3600000); //1 hour
+      const expiryDate = new Date(Date.now() + 3600000); // 1 hour
       res
         .cookie("access_token", token, {
           httpOnly: true,
@@ -73,4 +73,8 @@ export const google = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+export const signout = (req, res) => {
+  res.clearCookie("access_token").status(200).json("Signout success!");
 };
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -45,6 +45,11 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    signOut: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = false;
+    },
   },
 });
 
@@ -58,6 +63,7 @@ export const {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOut,
 } = userSlice.actions;
 
 export default userSlice.reducer;
