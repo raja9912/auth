9c7717a399feabf82ba930da98e4a365847424b4
raja9912 <rajasimhareddy02.com>
2024-04-15 20:43:05 -0500

    edit

--- api/index.js
+++ api/index.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-
 dotenv.config();
 
 mongoose
@@ -15,20 +14,19 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
 const app = express();
+
 app.use(express.json());
 
 app.use(cookieParser());
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
 
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
-
 app.use((err, req, res, next) => {
-  const statusCode = err.statuscode || 500;
+  const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
